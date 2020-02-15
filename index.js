e356e3609902ea44ec882ea987a2c35d466a9dd6
Ryo KUNO <kuno@aise.ics.saitama-u.ac.jp>
2020-02-15 16:18:01 +0900

    html miss?

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const port = 3000;
+const hostname = '127.0.0.1';
 
+var server = http.createServer();
+server.on('request', doRecquest);
+
+var fs = require('fs');
+
+function doRecquest(req,res){
+    fs.readFile('./index.html', 'utf-8', doReard);
+
+    function doReard(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end();
+    }
+}
+
+server.listen(port, hostname, ()=>{
+    console.log('Server running at http://${hostname} : ${port}/');
+});
 
 app.get('/', (req,res) => res.send("hello world"));
 
@@ -13,6 +33,8 @@ app.get('/api/v1/ppap', (req,res) => {
     ];
     res.json(ppap);
 })
+
+
 
 app.listen(3000, () => console.log('Listening on port 3000'));
 
